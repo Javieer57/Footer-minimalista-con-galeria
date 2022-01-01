05e3050d839a8f19b19e9c2c1c834ec723f8f22b
Javieer E <javieer.eufracio@gmail.com>
2022-01-01 16:20:35 -0600

    Iniciar repositorio

---
+++ app/js/script.js
@@ -0,0 +1,24 @@
+/* Reemplazar los data-feather="" por iconos SVG */
+feather.replace();
+
+/* Ejecutar la librería de Owl carousel
+
+Fuente: https://owlcarousel2.github.io/OwlCarousel2/docs/started-welcome.html */
+$(document).ready(function () {
+	$(".owl-carousel").owlCarousel({
+		center: true,
+		items: 2,
+		loop: true,
+		autoplay: true,
+		autoplayTimeout: 3000,
+		autoplayHoverPause: true,
+		responsive: {
+			600: {
+				items: 4,
+			},
+			1300: {
+				items: 5,
+			},
+		},
+	});
+});
